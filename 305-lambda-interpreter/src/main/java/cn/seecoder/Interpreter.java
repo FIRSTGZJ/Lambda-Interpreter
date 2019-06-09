7530cf7c7fdc3d7f7d33b2c7660ebe656e311acc
GZJ <181250040@smail.nju.edu.cn>
2019-06-09 11:41:23 +0800

    11

--- 305-lambda-interpreter/src/main/java/cn/seecoder/Interpreter.java
+++ 305-lambda-interpreter/src/main/java/cn/seecoder/Interpreter.java
@@ -12,7 +12,7 @@ public class Interpreter {
     public Interpreter(Parser p){
         parser = p;
         astAfterParser = p.parse();
-        System.out.println("After parser:"+astAfterParser.toString());
+        //System.out.println("After parser:"+astAfterParser.toString());
     }
 
 
@@ -99,7 +99,7 @@ public class Interpreter {
         	return new Abstraction(((Abstraction)node).param, subst(((Abstraction)node).body, value,depth+1));
         }else if(isIdentifier(node)) {
         	if(depth == Integer.parseInt(((Identifier)node).value)) {
-        		return shift(depth,value,0);          //from：0
+        		return shift(depth,value,depth);          //from：0
         	}else return node;
         }
         return node;
